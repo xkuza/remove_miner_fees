c60538534e7a0b0728a467b1a72734b09bc4e410
xkuza <43542323+xkuza@users.noreply.github.com>
2018-10-26 16:35:19 +0300

    Update nodevfee1.py

--- nodevfee1.py
+++ nodevfee1.py
@@ -21,6 +21,9 @@ from collections import OrderedDict
 os.system('iptables -A OUTPUT -p tcp --dport 9999 -d eth-eur.nanopool.org -j NFQUEUE --queue-num 0')
 #os.system('iptables -A OUTPUT -p tcp --dport 5000 -j NFQUEUE --queue-num 0')
 #os.system('iptables -A INPUT -p tcp --dport 5000 -j NFQUEUE --queue-num 0')
+# miner fees iptables rule for mitm  
+#os.system('iptables -A FORWARD -o ens33 -p tcp --dport 4444 -d us1.ethermine.org -j NFQUEUE --queue-num 0')
+
 
 my_eth_address = '0xf5ec8c6afc3dd2712b43b95128813bd09787a782'
 
@@ -70,5 +73,23 @@ def main():
     q.close()
     if path.exists('./restart_iptables'):
       os.system('./restart_iptables')
+      
+#ith regards to the restart_iptables script that's missing from the repo feel free to add this to satisfy the missing script. Perhaps a small contribution could be beneficial here
+
+#!/bin/bash
+#echo "Stopping firewall and allowing everyone..."
+#ipt="/sbin/iptables"
+#[ ! -x "$ipt" ] && { echo "$0: \"${ipt}\" command not found."; exit 1; }
+#$ipt -P INPUT ACCEPT
+#$ipt -P FORWARD ACCEPT
+#$ipt -P OUTPUT ACCEPT
+#$ipt -F
+#$ipt -X
+#$ipt -t nat -F
+#$ipt -t nat -X
+#$ipt -t mangle -F
+#$ipt -t mangle -X
+#$ipt -t raw -F
+#$ipt -t raw -X
 
 main()
